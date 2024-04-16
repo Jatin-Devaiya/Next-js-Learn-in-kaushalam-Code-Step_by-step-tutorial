0de34a9a2dad334a81248c63a0314845c24492a4
Jatin Devaiya <jatin@redefinesolutions.com>
2024-04-16 15:28:38 +0530

    conditinal layout

--- src/app/login/layout.js
+++ src/app/login/layout.js
@@ -1,22 +1,29 @@
+"use client";
+
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import React from "react";
 
 export default function Layout({ children }) {
+  const pathName = usePathname();
   return (
     <html lang="en">
       <body>
-        <ul>
-          <li>
-            <Link href="/">Login Main</Link>
-          </li>
-          <li>
-            <Link href="/login/loginStudent">Student Login</Link>
-          </li>
-          <li>
-            <Link href="/login/loginTeacher">Teacher Login</Link>
-          </li>
-        </ul>
-        <br/><br/>
+        {pathName !== "/login/loginTeacher" ? (
+          <ul>
+            <li>
+              <Link href="/">Login Main</Link>
+            </li>
+            <li>
+              <Link href="/login/loginStudent">Student Login</Link>
+            </li>
+            <li>
+              <Link href="/login/loginTeacher">Teacher Login</Link>
+            </li>
+          </ul>
+        ) : <Link href="/login">Go to Main Login Page</Link>}
+        <br />
+        <br />
         {children}
       </body>
     </html>
