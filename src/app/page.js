86c57440f27bfac30b203fae4f6594fed0ad7f5d
Jatin Devaiya <jatin@redefinesolutions.com>
2024-04-18 14:58:01 +0530

    module css and conditional style

--- src/app/page.js
+++ src/app/page.js
@@ -1,29 +1,43 @@
-'use client'
+"use client";
 
 import Link from "next/link";
 import { useState } from "react";
+import custom from "./custom.module.css";
+import other from "./other.module.css";
+import style from "./style.module.css";
 
 export default function Home() {
-  const [name, setname] = useState("Raj")
-  const apple = ()=>{
-    setname("kavi")
-  }
+  const [name, setname] = useState("Raj");
+  const [color, setcolor] = useState("red");
+
+  const apple = () => {
+    setname("kavi");
+  };
+
   return (
     <div className="">
-      <div>Home Page</div>
-      <Link href={"/login"}>Go to Login Page</Link> {" "}
+      <div className={custom.main}>Home Page</div>
+      <div className={other.main}>Hello How are you!</div>
+      <div className={color=="red"?style.red:style.green}>I am Fine</div>
+      <button onClick={()=>setcolor("gray")}>change color</button>
+      <br />
+
+      <Link href={"/login"}>Go to Login Page</Link>{" "}
       <Link href={"/about"}>Go to About Page</Link>
       <div>My name is {name}</div>
-      <Name name="jatin" surname="Devaiya"/>
-      <Name name="Hevin"/>
-
-      <button onClick={()=>apple()} className="hover:underline">Click  Me</button>
+      <Name name="jatin" surname="Devaiya" />
+      <Name name="Hevin" />
+      <button onClick={() => apple()} className="hover:underline">
+        Click Me
+      </button>
     </div>
   );
 }
 
-const Name= (pros)=>{
-  return(
-    <h1>My Name is {pros.name}{" "}{pros.surname}</h1>
-  )
-}
+const Name = (pros) => {
+  return (
+    <h1>
+      My Name is {pros.name} {pros.surname}
+    </h1>
+  );
+};
