13408a9a403a359711249ba754c93c0b7ec385e2
Jatin Devaiya <jatin@redefinesolutions.com>
2024-04-19 12:15:58 +0530

    Loading Feature in next js

---
+++ src/app/user/loading.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className="text-rose-600 text-center">Loading page..........</div>
+  )
+}
+
+export default Loading
--- src/app/user/page.jsx
+++ src/app/user/page.jsx
@@ -16,22 +16,48 @@
 //     }
 // }
 
-"use client";
+// "use client";
 
-import Script from "next/script";
+// import Script from "next/script";
+// import React from "react";
+
+// const page = () => {
+//   return (
+//     <div>
+//       <Script
+//         src="/location.js"
+//         onLoad={() => {
+//           console.log("file Loaded");
+//         }}
+//       />
+//     </div>
+//   );
+// };
+
+// export default page;
+
 import React from "react";
 
-const page = () => {
+async function userList() {
+  let data = await fetch("https://dummyjson.com/users");
+  data = await data.json();
+  return data.users;
+}
+
+const User = async () => {
+  const alluser = await userList();
   return (
     <div>
-      <Script
-        src="/location.js"
-        onLoad={() => {
-          console.log("file Loaded");
-        }}
-      />
+      <h1>User List</h1>
+      {alluser.map((user) => {
+        return (
+          <>
+            <h1> Name : {user?.firstName}</h1>
+          </>
+        );
+      })}
     </div>
   );
 };
 
-export default page;
+export default User;
