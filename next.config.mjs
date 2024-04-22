915fe0899ada3b00d40520972a5ebfb2a8ca98ad
Jatin Devaiya <jatin@redefinesolutions.com>
2024-04-22 11:55:58 +0530

    redirects configuration

--- next.config.mjs
+++ next.config.mjs
@@ -2,7 +2,21 @@
 const nextConfig = {
     images:{
         domains:["cdn.dummyjson.com",]
-    }
+    },
+   redirects:async()=>{
+    return [
+        {
+            source:"/user",
+            destination:"/",
+            permanent:false,
+        },
+        {
+            source:"/user/:userId",
+            destination:"/",
+            permanent:false,
+        }
+    ]
+   } 
 };
 
 export default nextConfig;
