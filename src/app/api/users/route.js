e71602b43d2f28cba14c0908769c7e18b9b8aef8
Jatin Devaiya <jatin@redefinesolutions.com>
2024-04-23 11:18:28 +0530

    POST API with use postman with API routes

--- src/app/api/users/route.js
+++ src/app/api/users/route.js
@@ -5,3 +5,17 @@ export function GET() {
   const data = user;
   return NextResponse.json(data, { status: 200 });
 }
+
+export async function POST(request) {
+  let payload = await request.json();
+  if (!payload.name || !payload.age || !payload.email) {
+    return NextResponse.json(
+      { result: "field is requires", success: false },
+      { status: 400 }
+    );
+  }
+  return NextResponse.json(
+    { result: "new user created", success: true },
+    { status: 201 }
+  );
+}
