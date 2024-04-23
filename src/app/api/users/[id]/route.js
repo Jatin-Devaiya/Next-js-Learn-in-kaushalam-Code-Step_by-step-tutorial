d21fdaad3f021c3cebb9fef423c8782df16f3901
Jatin Devaiya <jatin@redefinesolutions.com>
2024-04-23 15:56:34 +0530

    PUT API make with api routes v-40

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -2,12 +2,27 @@ import { user } from "@/util/db";
 import { NextResponse } from "next/server";
 
 export function GET(req, res, next) {
-  console.log(res.params.id, "res");
+  // console.log(res.params.id, "res");
   const userData = user.filter((item) => {
     return item.id == res.params.id;
   });
+  return NextResponse.json(
+    userData.length == 0 ? "No data Found" : userData[0],
+    {
+      status: 200,
+    }
+  );
+}
 
-  return NextResponse.json(userData.length == 0 ? "No data Found" : userData[0], {
-    status: 200,
-  });
+export async function PUT(req, res, next) {
+  let payload = await req.json();
+  payload.id = res.params.id;
+  console.log(payload);
+  if (!payload.id || !payload.name || !payload.email || !payload.age) {
+    return NextResponse.json(
+      { result: "request data is not valid", success: false },
+      { status: 400 }
+    );
+  }
+  return NextResponse.json({ result: payload, success: true }, { status: 200 });
 }
