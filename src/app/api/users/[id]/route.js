349cc421ed4ffa5881f659b126caed2575982c2b
Jatin Devaiya <jatin@redefinesolutions.com>
2024-04-23 19:05:18 +0530

    DELETE API make and use with postman v-42

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -26,3 +26,19 @@ export async function PUT(req, res, next) {
   }
   return NextResponse.json({ result: payload, success: true }, { status: 200 });
 }
+
+export function DELETE(request, content) {
+  console.log(request,"resquest",content,"content");
+  let id = content.params.id;
+  if (id) {
+    return NextResponse.json(
+      { result: "user deleted", success: true },
+      { status: 200 }
+    );
+  } else {
+    return NextResponse.json(
+      { result: "Plaese try after some time", success: false },
+      { status: 400 }
+    );
+  }
+}
